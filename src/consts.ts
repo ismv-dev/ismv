e2d3c1d3d76dae6a6c10ad08e4b139f81655ea9a
ismv-dev <igalindo199@gmail.com>
2026-06-29 13:29:29 -0400

    curriculum

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Ismael Galindo';
+export const SITE_DESCRIPTION = 'Currículum personal de Ismael Galindo, estudiante de Ingeniería en Informática especializado en desarrollo Full Stack e IA.';
